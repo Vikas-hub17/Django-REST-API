10d5967a4ba66403e310327bffda185655f03727
VIKAS <vikasraw17@gmail.com>
2024-09-28 11:59:26 +0530

    commit

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'allauth.account.middleware.AccountMiddleware',
      'users.middleware.LogRequestsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
