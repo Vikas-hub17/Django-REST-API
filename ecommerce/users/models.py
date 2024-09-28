43443306b8b6b03aefafd95bbedbc491d89d5da2
VIKAS <vikasraw17@gmail.com>
2024-09-28 12:07:24 +0530

    commit

--- ecommerce/users/models.py
+++ ecommerce/users/models.py
@@ -1,8 +1,19 @@
-from django.db import models
-
-# Create your models here.
+from django.db import models  # <-- Add this import
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',  # Set a unique related_name for groups
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions_set',  # Set a unique related_name for user_permissions
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
